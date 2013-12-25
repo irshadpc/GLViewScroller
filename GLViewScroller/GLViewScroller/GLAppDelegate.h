6519337fa68563e0e3507ebe3b1b5ab8acf5f1fb
Gertjan Leemans <gertjan@digitalisma.com>
2013-12-25 10:43:51 +0100

    Created initial project

---
+++ GLViewScroller/GLViewScroller/GLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GLAppDelegate.h
+//  GLViewScroller
+//
+//  Created by Gertjan Leemans on 25/12/13.
+//  Copyright (c) 2013 Gertjan Leemans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GLViewScroller/GLViewScroller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GLViewScroller
+//
+//  Created by Gertjan Leemans on 25/12/13.
+//  Copyright (c) 2013 Gertjan Leemans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GLAppDelegate class]));
+    }
+}
